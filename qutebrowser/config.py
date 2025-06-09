aca930429a5260e0ce214f6146af79092eeca205
gamesmessiah <31961404+gamesmessiah@users.noreply.github.com>
2025-06-09 18:37:29 +0100

    Update config.py

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -1,1 +1,17 @@
+import dracula.draw
 
+# Load existing settings made via :set
+config.load_autoconfig()
+
+dracula.draw.blood(c, {
+    'spacing': {
+        'vertical': 6,
+        'horizontal': 8
+    }
+})
+
+# Bind 'Ctrl-Shift-m' to spawn mpv with the current URL
+config.bind('M', 'spawn --userscript mpv {url}')
+
+# Bind 'Ctrl-m' to hint links and spawn mpv for the selected link
+config.bind('<Ctrl-m>', 'hint links spawn mpv {hint-url}')
